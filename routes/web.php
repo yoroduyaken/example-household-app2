727032e14ba832623b8cab5edd1d77f29b7ec7b0
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-06-27 23:45:22 +0900

    homeが表示されるようにroutesを追加した

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('top');
 })->name('top');
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
