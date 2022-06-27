6056d77f129f76b44a9d1738c960c03f64d47936
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-06-28 00:26:15 +0900

    フォームからの情報をテーブルに挿入した

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -11,8 +13,13 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
         return redirect()->route('home');
     }
 }
